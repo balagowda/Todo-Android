fe0e8c0ce3840fe76970c731227612a52fbb87c9
balagowda <balagowda9483@gmail.com>
2023-09-20 21:44:59 +0530

    todo addition implementation in java

---
+++ app/src/main/java/com/example/mytask/Adopter/TodoAdopter.java
@@ -0,0 +1,51 @@
+package com.example.mytask.Adopter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mytask.MainActivity;
+import com.example.mytask.Model.ToDoModel;
+import com.example.mytask.R;
+
+public class TodoAdopter extends RecyclerView.Adapter<TodoAdopter.ViewHolder> {
+
+    private List<ToDoModel> todoList;
+    private MainActivity activity;
+
+    public TodoAdopter(MainActivity activity){
+        this.activity=activity;
+    }
+
+    public ViewHolder onCreateViewHolder(ViewGroup parent,int viewType){
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.task_layout, parent, false);
+        return  new ViewHolder(itemView);
+    }
+
+    public void onBindViewHolder( ViewHolder holder, int position) {
+        ToDoModel item = todoList.get(position);
+        holder.task.setText(item.getTask());
+        holder.task.setChecked(toBoolean(item.getStatus()));
+    }
+
+    public int getItemCount(){
+        return todoList.size();
+    }
+
+    private boolean toBoolean(int n){
+        return n!=0;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        CheckBox task;
+        ViewHolder(View view){
+            super(view);
+            task = view.findViewById(R.id.todoCheckBox);
+        }
+    }
+}
--- app/src/main/java/com/example/mytask/MainActivity.java
+++ app/src/main/java/com/example/mytask/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.mytask;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.mytask.Adopter.TodoAdopter;
+import com.example.mytask.Model.ToDoModel;
+
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView taskRecyclerView;
+    private TodoAdopter taskAdopter;
+    private List<ToDoModel> taskList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        taskRecyclerView = findViewById(R.id.tasksRecyclerView);
+        taskRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        taskAdopter = new TodoAdopter(this);
+        taskRecyclerView.setAdapter(taskAdopter);
+
+        ToDoModel task = new ToDoModel();
+        task.setTask("This is a task list");
+        task.setStatus(0);
+        task.setId(1);
+
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+
+
     }
 }
