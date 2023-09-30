6560081e2ccb7c003a8448a9f6926bf94ca09576
balagowda <balagowda9483@gmail.com>
2023-09-30 12:56:45 +0530

    Bug fixing and error solving

--- app/src/main/java/com/example/mytask/Utils/Databasehandler.java
+++ app/src/main/java/com/example/mytask/Utils/Databasehandler.java
@@ -1,66 +1,65 @@
 package com.example.mytask.Utils;
 
-import android.annotation.SuppressLint;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.os.Build;
-import android.os.ConditionVariable;
+
 
 import com.example.mytask.Model.ToDoModel;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Databasehandler  extends SQLiteOpenHelper {
+public class Databasehandler extends SQLiteOpenHelper {
+
     private static final int VERSION = 1;
-    private static final String NAME = "toDOListDatabase";
+    private static final String NAME = "toDoListDatabase";
     private static final String TODO_TABLE = "todo";
     private static final String ID = "id";
     private static final String TASK = "task";
     private static final String STATUS = "status";
-    private static final String CREATE_TODO_TABLE = "CREATE TABLE "+ TODO_TABLE + "("+ ID +" INTEGER PRIMARY KEY AUTOINCREMENT, "+ TASK + "TEXT, "+STATUS+"INTEGER)";
+    private static final String CREATE_TODO_TABLE = "CREATE TABLE " + TODO_TABLE + "(" + ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + TASK + " TEXT, "
+            + STATUS + " INTEGER)";
 
     private SQLiteDatabase db;
 
-    public Databasehandler(Context context){
-        super(context ,NAME,null,VERSION);
+    public Databasehandler(Context context) {
+        super(context, NAME, null, VERSION);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase db){
+    public void onCreate(SQLiteDatabase db) {
         db.execSQL(CREATE_TODO_TABLE);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db,int oldVersion,int newVersion){
-        //Drop the older table
-        db.execSQL("DROP TABLE IF EXISTS "+TODO_TABLE);
-        //CREATE TABLES AGIAN
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Drop older table if existed
+        db.execSQL("DROP TABLE IF EXISTS " + TODO_TABLE);
+        // Create tables again
         onCreate(db);
     }
 
-    public void openDatabase(){
+    public void openDatabase() {
         db = this.getWritableDatabase();
     }
 
     public void insertTask(ToDoModel task){
         ContentValues cv = new ContentValues();
-        cv.put(TASK,task.getTask());
-        cv.put(STATUS,0);
-        db.insert(TODO_TABLE,null,cv);
+        cv.put(TASK, task.getTask());
+        cv.put(STATUS, 0);
+        db.insert(TODO_TABLE, null, cv);
     }
 
-    @SuppressLint("Range")
     public List<ToDoModel> getAllTasks(){
         List<ToDoModel> taskList = new ArrayList<>();
         Cursor cur = null;
         db.beginTransaction();
         try{
-            cur = db.query(TODO_TABLE,null,null,null,null,null,null,null);
-            if(cur!=null){
+            cur = db.query(TODO_TABLE, null, null, null, null, null, null, null);
+            if(cur != null){
                 if(cur.moveToFirst()){
                     do{
                         ToDoModel task = new ToDoModel();
@@ -68,29 +67,32 @@ public class Databasehandler  extends SQLiteOpenHelper {
                         task.setTask(cur.getString(cur.getColumnIndex(TASK)));
                         task.setStatus(cur.getInt(cur.getColumnIndex(STATUS)));
                         taskList.add(task);
-                    }while (cur.moveToNext());
+                    }
+                    while(cur.moveToNext());
                 }
             }
         }
         finally {
-        db.endTransaction();
+            db.endTransaction();
+            assert cur != null;
+            cur.close();
         }
         return taskList;
     }
 
-    public void updateStatus(int id,int status){
+    public void updateStatus(int id, int status){
         ContentValues cv = new ContentValues();
-        cv.put(STATUS,status);
-        db.update(TODO_TABLE,cv,ID+"=?",new String[]{String.valueOf(id)});
+        cv.put(STATUS, status);
+        db.update(TODO_TABLE, cv, ID + "= ?", new String[] {String.valueOf(id)});
     }
 
-    public void updateTask(int id,String task){
+    public void updateTask(int id, String task) {
         ContentValues cv = new ContentValues();
-        cv.put(TASK,task);
-        db.update(TODO_TABLE,cv,ID+"=?",new String[]{String.valueOf(id)});
+        cv.put(TASK, task);
+        db.update(TODO_TABLE, cv, ID + "= ?", new String[] {String.valueOf(id)});
     }
 
     public void deleteTask(int id){
-        db.delete(TODO_TABLE,ID+"=?",new String[]{String.valueOf(id)});
+        db.delete(TODO_TABLE, ID + "= ?", new String[] {String.valueOf(id)});
     }
 }
